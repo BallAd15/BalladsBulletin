133fee4540c326dad08fde2e78d0674aca634899
BallAd15 <aditya22bcs54@iiitkottayam.ac.in>
2023-12-23 21:01:33 +0530

    My First commit

---
+++ lib/pages/article_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleView extends StatefulWidget {
+  String blogUrl;
+  ArticleView({required this.blogUrl});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Flutter"),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        centerTitle: true, // Place it in center
+        elevation: 0.0, //Remove any default elevation given by appbar
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
